1f1dc10d09cb0567f731805a07ceec5d5dff5d54
Tanguy Leconte <taleconte@gmail.com>
2023-03-09 16:48:47 +0100

    Pass exercice 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let args = Array.from(arguments);
+    args.shift();
+    let finalArray = [];
+    outter: 
+    for (let arr of array){
+        for(let arg of args){
+            if(arr === arg){
+                continue outter;
+            }
+        }
+        finalArray.push(arr);
+    }
 
+    return finalArray;
 };
 
+//console.log(removeFromArray([1, 2, 3, 4, 'hey'], 3, 4, 'hey'))
 // Do not edit below this line
 module.exports = removeFromArray;
