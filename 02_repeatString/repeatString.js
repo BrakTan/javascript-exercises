d512ba174ccff0a2fc5dd35e81e2f2f57d587c83
Tanguy Leconte <taleconte@gmail.com>
2023-03-09 15:47:38 +0100

    Pass exercice 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(message, numberOfTime) {
+    let finalMessage = "";
+    if(numberOfTime < 0) return "ERROR";
+    for(let i = 0; i < numberOfTime; i++){
+        finalMessage += message;
+    }
+    return finalMessage;
 };
 
 // Do not edit below this line
