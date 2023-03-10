bfd76f51c924c502e5cf66ca08501c83d2c2bde5
Tanguy Leconte <taleconte@gmail.com>
2023-03-10 15:20:10 +0100

    Pass exercice 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    let sum = 0;
+    let begin;
+    let end;
+    /*let args = Array.from(arguments);
+    args.forEach((element) => {
+        if(element < 0 || typeof element != 'number'){
+            return "ERROR";
+        }
+    });*/
+    if(first < 0 || second < 0 || typeof first != 'number'
+    || typeof second != "number"){
+        return "ERROR";
+    }
+    if(first < second){
+        begin = first;
+        end = second;
+    }
+    else if(first > second){
+        begin = second;
+        end = first;
+    }
+    else{
+        return first+second;
+    }
+    for(let i = begin; i <= end; i++){
+        sum += i;
+    }
+    return sum;
 };
-
+console.log(sumAll(4, '8'))
 // Do not edit below this line
 module.exports = sumAll;
