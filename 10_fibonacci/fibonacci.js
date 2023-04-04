7a5f52d65825fa19524afa8a038a8e6497b75147
Tanguy Leconte <taleconte@gmail.com>
2023-04-04 14:54:39 +0200

    Pass exercice 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    let number = +a;
+    if(a <= 0){
+        return 'OOPS';
+    }
+    let old = 0;
+    let newer = 0;
+    let result = 1;
+    for(let i = a; i > 1; i--){
+        old = newer;
+        newer = result;
+        result = old + newer;
+    }
+    return result;
 };
 
 // Do not edit below this line
