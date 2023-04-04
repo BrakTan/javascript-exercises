87515e0323f88019c70e2492774082268f92ff6f
Tanguy Leconte <taleconte@gmail.com>
2023-04-04 14:35:02 +0200

    Pass exercice 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,11 @@
-const palindromes = function () {
+const palindromes = function (stringArg) {
+    const ponctuation = [' ', '!','?',',','.'];
+    let a = Array.from(stringArg).filter(letter => {
+        return !ponctuation.includes(letter);
+    });
+    let c = a.join('').toLowerCase();
+    let b = a.reverse().join('').toLowerCase();
+    return c == b;
 
 };
 
