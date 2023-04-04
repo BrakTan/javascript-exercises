6730aa37542f866674840862aff414b68ddbcf63
Tanguy Leconte <taleconte@gmail.com>
2023-04-04 14:02:01 +0200

    Pass the exercice 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let somme = 0;
+	const args = Array.from(a);
+  args.forEach(arg => {
+    somme += arg;
+  })
+  return +somme;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let somme = 1;
+	const args = Array.from(a);
+  args.forEach(arg => {
+    somme *= arg;
+  })
+  return somme;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let fact = a;
+  let result = a;
+	if(a <= 1){
+    return 1;
+  }
+  return result * factorial(fact - 1);
 };
 
 // Do not edit below this line
