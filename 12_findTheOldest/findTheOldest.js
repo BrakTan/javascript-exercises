918d01644ea584c0f9f784c5cc28e024b032e896
Tanguy Leconte <taleconte@gmail.com>
2023-04-06 11:10:23 +0200

    Pass exercice 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(array) {
+    const oldest = array.reduce((older, next) => {
+        if(!older.yearOfDeath){
+            older.yearOfDeath = (new Date).getFullYear();
+        }
+        if(!next.yearOfDeath){
+            next.yearOfDeath = (new Date).getFullYear();
+        }
+        const olderAge = older.yearOfDeath - older.yearOfBirth;
+        const nextAge = next.yearOfDeath - next.yearOfBirth;
+        return  nextAge > olderAge ? next : older;
+    },)
+    return oldest;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
